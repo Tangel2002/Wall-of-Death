4c6044f0b3854eec3895aeb40b3a88ce188e68df
Maddog4302 <tangel@ltu.edu>
2022-01-25 11:13:36 -0500

    setup rolling
    rolling velocity needs to be capped

---
+++ Assets/Scripts/Roll.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Roll : MonoBehaviour
+{
+
+
+    public Rigidbody player;
+    public float speed = 10;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("d"))
+        {
+            player.AddForce(Vector3.right * speed);
+        }
+        if (Input.GetKey("a"))
+        {
+            player.AddForce(Vector3.left * speed);
+            player.velocity.Normalize();
+        }
+    }
+}
