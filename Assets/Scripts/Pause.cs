32ef53fe31ff3a7ffe6338093eae14c3cd83cfd3
Tangel2002 <tangel@ltu.edu>
2022-01-26 10:55:15 -0500

    addded pause menu

---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Pause : MonoBehaviour
+{
+
+    bool toggle = false;
+    public GameObject canv;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            if (toggle)
+            {
+                Resume();
+            }
+            if (!toggle)
+            {
+                Paused();
+            }
+        }
+    }
+
+    public void Paused()
+    {
+        toggle = true;
+        Time.timeScale = 0.25f;
+        canv.SetActive(true);
+    }
+    public void Resume()
+    {
+        toggle = false;
+        Time.timeScale = 1;
+        canv.SetActive(false);
+    }
+    public void loadUp(string scene)
+    {
+        SceneManager.LoadScene(scene);
+        Time.timeScale = 1;
+    }
+    public void quit()
+    {
+        Application.Quit();
+    }
+}
